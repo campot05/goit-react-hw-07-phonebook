3f0e6ee978e17468cd8da570a1eb2ee8bb0d760d
Daniel <daniil.spectr@gmail.com>
2023-03-12 15:41:05 +0200

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -31,7 +31,6 @@ export const addContact = createAsyncThunk(
   async (data, { rejectWithValue }) => {
     try {
       const result = await api.addContact(data);
-      console.log(result);
       return result;
     } catch (error) {
       return rejectWithValue(error);
